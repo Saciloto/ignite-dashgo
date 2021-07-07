fa66e4580628b06bb5301cfb18c5ee185b086608
Ssciloto <mauriciosaciloto@gmail.com>
2021-07-07 10:24:12 -0300

    End of chapter

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,19 +1,22 @@
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
 import React from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+export function Input({ name, label, error = null, ...rest }: InputProps) {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor="email">{label}</FormLabel>}
 
       <ChakraInput
@@ -28,6 +31,7 @@ export function Input({ name, label, ...rest }: InputProps) {
         size="lg"
         {...rest}
       />
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
 }
